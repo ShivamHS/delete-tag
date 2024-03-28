0e30b77aaa14ddd15d67e4299dba9abb43bdd1bb
ShivamHS <140150882+ShivamHS@users.noreply.github.com>
2024-03-28 14:09:10 +0000

    Update delete-tag.ps1

--- delete-tag.ps1
+++ delete-tag.ps1
@@ -1,23 +1,18 @@
 $token = $env:INPUT_TOKEN
 $repo = $env:INPUT_REPO
 $keyword = $env:INPUT_KEYWORD
+$owner = $env:INPUT_OWNER
 
-
- Write-Host "repo: $repo"
- Write-Host "keyword: $keyword"
-
-
-$tagsUrl = "https://api.github.com/repos/$repo/git/refs/tags"
+$tagsUrl = "https://api.github.com/repos/$owner/$repo/git/refs/tags"
 $headers = @{
     "Authorization" = "token $token"
     "User-Agent" = "PowerShell-GitHub-Tag-Deletion"
 }
 
- 
+Write-Host "$tagsUrl"
 
 $response = Invoke-RestMethod -Uri $tagsUrl -Headers $headers -Method Get
 
-Write-Host "tagurl: $tagsurl"
  
 
 foreach ($tag in $response) {
@@ -28,6 +23,7 @@ foreach ($tag in $response) {
     
     if ($tagName -like "*$keyword*") {
         
+        $deleteUrl = "https://api.github.com/repos/$owner/$repo/git/$tagName"
         Invoke-RestMethod -Uri $tagname -Headers $headers -Method Delete
         Write-Host "Deleted tag: $tagName"
     }
